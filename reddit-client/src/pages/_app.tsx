592202c313002f28dc1cfbdf2badf837f4ba3a4b
Xavier San Lorenzo <sanlorenzoxavier@gmail.com>
2021-01-12 21:01:38 +0800

    init

--- reddit-client/src/pages/_app.tsx
+++ reddit-client/src/pages/_app.tsx
@@ -7,19 +7,19 @@ import {
   dedupExchange,
   fetchExchange,
 } from "urql";
-import { cacheExchange } from "@urql/exchange-graphcache";
+// import { cacheExchange } from "@urql/exchange-graphcache";
 
 import theme from "../theme";
 import { LogInUserMutation, MeDocument } from "../generated/graphql";
 
-function betterUpdateQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
+// function betterUpdateQuery<Result, Query>(
+//   cache: Cache,
+//   qi: QueryInput,
+//   result: any,
+//   fn: (r: Result, q: Query) => Query
+// ) {
+//   return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+// }
 
 const client = createClient({
   url: "http://localhost:5001/graphql",
